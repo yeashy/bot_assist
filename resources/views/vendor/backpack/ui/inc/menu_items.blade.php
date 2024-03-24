68faac49a21cbc2782c6bbd61da1f60623c353c1
yeashy <gertyn07@gmail.com>
2024-03-24 16:28:51 +0700

    Сделал меню более красивым

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -2,14 +2,17 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-item title="Компании" icon="la la-building" :link="backpack_url('company')" />
-<x-backpack::menu-item title="Типы компаний" icon="la la-list-ul" :link="backpack_url('company-type')" />
 
-<x-backpack::menu-item title="Шрифты" icon="la la-pen" :link="backpack_url('font')" />
+<x-backpack::menu-dropdown title="Справочники" icon="la la-list-ul">
+    <x-backpack::menu-dropdown-item title="Шрифты" icon="la la-pen" :link="backpack_url('font')" />
 
-<x-backpack::menu-item title="Клиенты" icon="la la-user" :link="backpack_url('client')" />
+    <x-backpack::menu-dropdown-item title="Типы компаний" icon="la la-list-ul" :link="backpack_url('company-type')" />
 
-<x-backpack::menu-item title="Сотрудники" icon="la la-address-card" :link="backpack_url('staff-member')" />
+    <x-backpack::menu-dropdown-item title="Сотрудники" icon="la la-address-card" :link="backpack_url('staff-member')" />
 
-<x-backpack::menu-item title="Услуги" icon="la la-handshake" :link="backpack_url('service')" />
+    <x-backpack::menu-dropdown-item title="Услуги" icon="la la-handshake" :link="backpack_url('service')" />
 
-<x-backpack::menu-item title="Должности" icon="la la-address-book" :link="backpack_url('job-position')" />
+    <x-backpack::menu-dropdown-item title="Должности" icon="la la-address-book" :link="backpack_url('job-position')" />
+</x-backpack::menu-dropdown>
+
+
